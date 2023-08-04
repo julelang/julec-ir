06ae5bd8231c85464c6a9a63fcdc5c0e2a56101e
Mertcan D <54983926+mertcandav@users.noreply.github.com>
2023-08-04 12:37:48 +0300

    Create ir-updater.sh

---
+++ updater/ir-updater.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/sh
+# Copyright 2023 The Jule Programming Language.
+# Use of this source code is governed by a BSD 3-Clause
+# license that can be found in the LICENSE file.
+
+mkdir ir_updater
+curl -o ir_updater/main.jule https://raw.githubusercontent.com/julelang/julec-ir/main/updater/main.jule
+julec -o ir_updater/updater ir_updater
+
+./ir_updater/updater
+
+if [ $? -eq 0 ]; then
+    echo "IRs updated successfully"
+else
+    echo "Some problem(s) occurred when IRs updating"
+fi
+
+echo "Cleaning..."
+
+rmdir -rf ir_updater
